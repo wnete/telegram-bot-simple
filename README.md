989ffa8048250a6fd4fe0f455612268d1b173718
wnete <38018218+wnete@users.noreply.github.com>
2018-04-10 17:01:31 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # telegram-bot-simple
+A sample telegram bot server in node.js
+
+see: [https://mvalipour.github.io/node.js/2015/11/10/build-telegram-bot-nodejs-heroku](https://mvalipour.github.io/node.js/2015/11/10/build-telegram-bot-nodejs-heroku)
+
+## start
+
+```
+npm install
+node .
+```
---
+++ bot.js
@@ -0,0 +1,25 @@
+var token = '575129053:AAEDPltJ_SjfG4xP4a6-ktjKFUQcT6KWSU0';
+
+var Bot = require('node-telegram-bot-api'),
+    bot = new Bot(token, { polling: true });
+
+console.log('bot server started...');
+
+// hello command
+bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+  var name = match[1];
+  bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
+    // reply sent!
+  });
+});
+
+// sum command
+bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
+  var result = 0;
+  match[1].trim().split(/\s+/).forEach(function (i) {
+    result += (+i || 0);
+  })
+  bot.sendMessage(msg.chat.id, result).then(function () {
+    // reply sent!
+  });
+});
